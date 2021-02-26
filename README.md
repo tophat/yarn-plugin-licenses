5a93d35736d78073e9558e1b74ee3b6c5c6355fa
Noah Negin-Ulster <noah.negin-ulster@tophatmonocle.com>
2021-02-26 08:12:38 -0500

    wip: working report

--- README.md
+++ README.md
@@ -1,5 +1,40 @@
 # yarn-plugin-licenses
 
+Yarn Berry plugin to enforce valid licenses used in a project.
+
+## Usage
+
+Define a `licenses.config.js` file:
+
+```js
+module.exports = {
+    isValidLicense: license => {
+        const valid = new RegExp('\\b(mit|apache\\b.*2|bsd|isc|unlicense)\\b', 'i')
+        return valid.test(license)
+    }
+}
+```
+
+or
+
+```js
+module.exports = {
+    isValidLicense: new RegExp('\\b(mit|apache\\b.*2|bsd|isc|unlicense)\\b', 'i'),
+    ignorePackages: ['react'],
+}
+```
+
+and then:
+
+```
+yarn import plugin @tophat/yarn-plugin-licenses
+yarn licenses audit --output-file=- --config=licenses.config.js
+```
+
+this outputs a junit report.
+
+You can use `yarn licenses audit --summary` for a human readable report for local dev.
+
 ## Contributors
 
 <!-- ALL-CONTRIBUTORS-LIST:START - Do not remove or modify this section -->
--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,77 @@ import {
     Cache,
     CommandContext,
     Configuration,
+    Ident,
     Manifest,
     Plugin,
     Project,
     StreamReport,
+    miscUtils,
 } from '@yarnpkg/core'
+import { npath, ppath } from '@yarnpkg/fslib'
 import { Command, Option, Usage } from 'clipanion'
+import junitBuilder from 'junit-report-builder'
+
+type LICENSE_FAILURE_TYPE = 'missing' | 'incompatible'
+
+const PRINTABLE_REASON: { [k in LICENSE_FAILURE_TYPE]: string } = {
+    missing: 'License could not be found.',
+    incompatible: 'License is incompatible.',
+}
+
+type LicenseCheckResult = {
+    reason?: LICENSE_FAILURE_TYPE
+    pass: boolean
+}
+
+type Result = {
+    reason?: LICENSE_FAILURE_TYPE
+    license?: string
+}
+
+type LicensePredicate = (license: string) => boolean
+
+class ResultMap<K, V> {
+    private _map: Map<K, V>
+    private _defaultValue: V
+
+    constructor(defaultValue: V) {
+        this._map = new Map<K, V>()
+        this._defaultValue = defaultValue
+    }
+
+    get(key: K): V {
+        if (!this._map.has(key)) {
+            this._map.set(key, this._defaultValue)
+        }
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        return this._map.get(key)!
+    }
+
+    set(key: K, value: V): void {
+        this._map.set(key, value)
+    }
+
+    has(key: K): boolean {
+        return this._map.has(key)
+    }
+
+    merge(key: K, value: Partial<V>): void {
+        const entry = this.get(key)
+        this.set(key, { ...entry, ...value })
+    }
+
+    entries(): IterableIterator<[K, V]> {
+        return this._map.entries()
+    }
+}
+
+const identToFullName = (ident: Ident): string => {
+    if (ident.scope) {
+        return `@${ident.scope}/${ident.name}`
+    }
+    return ident.name
+}
 
 class AuditLicensesCommand extends Command<CommandContext> {
     static paths = [['licenses', 'audit']]
@@ -20,14 +85,14 @@ class AuditLicensesCommand extends Command<CommandContext> {
         examples: [],
     })
 
-    outputFile?: string = Option.String('--output-file')
-    configFile?: string = Option.String('--config')
+    outputFile?: string = Option.String('--output-file', { required: false })
+    configFile?: string = Option.String('--config', { required: false })
     summary: boolean = Option.Boolean('--summary', false)
-    includeTransitive: boolean = Option.Boolean('--include-transitive', false)
+
+    ignorePackages = new Set<string>()
+    isValidLicensePredicate: LicensePredicate = () => false
 
     async execute(): Promise<number> {
-        // this.context.stdout.write('Running audit script...')
-
         const configuration = await Configuration.find(
             this.context.cwd,
             this.context.plugins,
@@ -37,15 +102,21 @@ class AuditLicensesCommand extends Command<CommandContext> {
 
         await project.restoreInstallState()
 
+        await this.parseConfigFile()
+
         const fetcher = await configuration.makeFetcher()
         const report = new StreamReport({
             stdout: new PassThrough(),
             configuration,
         })
 
-        for (const pkg of project.storedPackages.values()) {
-            // TODO: if includeTransitive is false, check if in workspace manifest / use different store to reduce iterable
+        const results = {
+            pass: new ResultMap<string, Result>({}),
+            fail: new ResultMap<string, Result>({}),
+            ignored: new ResultMap<string, Result>({}),
+        }
 
+        for (const pkg of project.storedPackages.values()) {
             const { packageFs, prefixPath } = await fetcher.fetch(pkg, {
                 project,
                 fetcher,
@@ -61,10 +132,78 @@ class AuditLicensesCommand extends Command<CommandContext> {
             const license = await this.parseLicense({
                 manifest,
             })
-            console.log(pkg.name, license)
+            const { reason, pass } = await this.isAllowableLicense({
+                license,
+            })
+            const result = { license: license || undefined, reason }
+
+            const fullName = identToFullName(pkg)
+            if (this.ignorePackages.has(fullName)) {
+                results.ignored.merge(fullName, result)
+            } else if (pass) {
+                results.pass.merge(fullName, result)
+            } else {
+                results.fail.merge(fullName, result)
+            }
+        }
+
+        const suite = junitBuilder.testSuite().name('Dependency Licenses Audit')
+        for (const [name, result] of [
+            ...results.pass.entries(),
+            ...results.fail.entries(),
+            ...results.ignored.entries(),
+        ].sort()) {
+            const testCase = suite.testCase().name(name)
+            if (results.ignored.has(name)) {
+                testCase.skipped()
+            } else if (result.reason) {
+                testCase.failure(
+                    `License: ${result.license}. Reason: ${
+                        PRINTABLE_REASON[result.reason]
+                    }`,
+                    result.reason,
+                )
+            }
+        }
+
+        if (this.outputFile) {
+            if (this.outputFile === '-') {
+                this.context.stdout.write(junitBuilder.build())
+            } else {
+                junitBuilder.writeTo(this.outputFile)
+            }
+        }
+
+        if (this.summary) {
+            this.context.stdout.write('Summary...')
         }
 
         return 0
+    }
+
+    async parseConfigFile(): Promise<void> {
+        if (!this.configFile) return
+
+        const configPPath = ppath.resolve(
+            ppath.cwd(),
+            npath.toPortablePath(this.configFile),
+        )
+        const config = miscUtils.dynamicRequireNoCache(configPPath)
+        const ignorePackages: string[] | undefined = config?.ignorePackages
+        if (ignorePackages) {
+            this.ignorePackages = new Set<string>(ignorePackages)
+        }
+        const isValidLicensePredicate = config?.isValidLicense
+        if (isValidLicensePredicate) {
+            if (typeof isValidLicensePredicate === 'function') {
+                this.isValidLicensePredicate = isValidLicensePredicate
+            } else if (isValidLicensePredicate instanceof RegExp) {
+                this.isValidLicensePredicate = (license: string) =>
+                    isValidLicensePredicate.test(license)
+            } else {
+                throw new Error('Invalid config option value: isValidLicense')
+            }
+        }
     }
 
     async parseLicense({
@@ -73,6 +212,25 @@ class AuditLicensesCommand extends Command<CommandContext> {
         manifest: Manifest
     }): Promise<string | null> {
         return manifest.license
+    }
+
+    async isMissing({ license }: { license: string }): Promise<boolean> {
+        const pattern = new RegExp('\\b(unknown|see license)\\b', 'i')
+        return pattern.test(license)
+    }
+
+    async isAllowableLicense({
+        license,
+    }: {
+        license: string | null
+    }): Promise<LicenseCheckResult> {
+        if (license && !(await this.isMissing({ license }))) {
+            if (this.isValidLicensePredicate(license)) {
+                return { pass: true }
+            }
+            return { pass: false, reason: 'incompatible' }
+        }
+        return { reason: 'missing', pass: false }
     }
 }
 
